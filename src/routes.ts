92362f51fd3112d20d401daef4a7dbf6eead7e81
caioesqueletron <caiocaio620@gmail.com>
2021-04-05 13:41:20 -0300

    finishing class

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,8 @@
 import {Request, Response} from 'express'
+import createUser from './services/CreateUser'
 export function helloWorld(request: Request, response: Response){
+    const user = createUser({name:'Caio', email:'caio@teste.com',password:'123',
+    techs: [{title:'nodeJS',experience: 130}]
+})
     return response.json({message: 'hello world'})
 }
